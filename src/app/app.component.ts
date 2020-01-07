29029e211c895e1441480dfa4d5ebd43d3b77808
Jimmy Svensson <jimmy888swe@gmail.com>
2020-01-07 22:55:29 +0100

    initt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  readonly ROOT_URL = 'http://localhost:5000';
   title = 'gerber';
+  constructor(private http: HttpClient) {}
+  gerber: Observable<any>;
+  getGerber() {
+
+    this.gerber = this.http.get(this.ROOT_URL + '/gerber');
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { InlineSVGModule } from 'ng-inline-svg';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    InlineSVGModule
   ],
   providers: [],
   bootstrap: [AppComponent]
